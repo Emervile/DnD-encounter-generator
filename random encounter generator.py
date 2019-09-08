0f5109e24b31d773c547cddcbdd153bd6094aa93
Emervile <54383612+Emervile@users.noreply.github.com>
2019-09-08 00:24:52 -0400

    Add files via upload

---
+++ random encounter generator.py
@@ -0,0 +1,38 @@
+import random
+
+creatures = ['1 Red Dragon', '20 Skeletons!', '13 Bandits', '15 Gnolls', '2 Wraiths', '10 Murlocks', '8 spiders', '3 werewolves', '3 elementals', '5 shadowhounds', '3 arcanians', 'Nothing!']
+dice_number = ['1', '2-10', '11-20', '21-30', '31-40', '41-50', '51-60', '61-70', '71-80', '81-90', '90-99', '100']
+encounter = list(zip(dice_number, creatures))
+
+print(encounter)
+
+def rolling_encounter():
+  roll = random.randint(1,100)
+  print('Rolling encounter')
+  print(roll)
+  if roll == 1:
+    return print('You have rolled a 1, now you must face ' + (creatures[0]) + '!')
+  elif roll <= 10:
+    return print('You have rolled, now you must face ' + (creatures[1]) + '!')
+  elif roll <= 20:
+    return print('You have rolled, now you must face ' + (creatures[2]) + '!')
+  elif roll <= 30:
+    return print('You have rolled, now you must face ' + (creatures[3]) + '!')
+  elif roll <= 40:
+    return print('You have rolled, now you must face ' + (creatures[4]) + '!')
+  elif roll <= 50:
+    return print('You have rolled, now you must face ' + (creatures[5]) + '!')
+  elif roll <= 60:
+    return print('You have rolled, now you must face ' + (creatures[6]) + '!')
+  elif roll <= 70:
+    return print('You have rolled, now you must face ' + (creatures[7]) + '!')
+  elif roll <= 80:
+    return print('You have rolled, now you must face ' + (creatures[8]) + '!')
+  elif roll <= 90:
+    return print('You have rolled, now you must face ' + (creatures[9]) + '!')
+  elif roll <= 99:
+    return print('You have rolled, now you must face ' + (creatures[10]) + '!')
+  elif roll == 100:
+    return print('You have rolled a 100, now you must face ' + (creatures[11]) + '!')
+
+rolling_encounter()
